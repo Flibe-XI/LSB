80f11e45724d8c928ad6e8d3706d6916e74db43c
Flibe <augshelakh@gmail.com>
2023-12-10 23:05:23 -0500

    amk9 rescue complete

---
+++ scripts/zones/Quicksand_Caves/npcs/amk_qm.lua
@@ -0,0 +1,12 @@
+-----------------------------------
+-- Area: Quicksand Caves
+-- NPC: qm (???)
+-- Mission: AMK - Rescue! A Moogle's Labor of Love
+-----------------------------------
+
+local entity = {}
+
+entity.onTrigger = function(player, npc)
+end
+
+return entity
