afe1d39b786fe180779b94bd7bf50966017d96b6
Flibe <augshelakh@gmail.com>
2024-10-10 21:11:38 -0400

    Cleared out old quest logic from 6 fishing related npcs

--- scripts/zones/Selbina/npcs/Zaldon.lua
+++ scripts/zones/Selbina/npcs/Zaldon.lua
@@ -563,30 +563,9 @@ local function giveReward(player, csid)
 end
 
 entity.onTrade = function(player, npc, trade)
-    local underTheSea    = player:getQuestStatus(xi.questLog.OTHER_AREAS, xi.quest.id.otherAreas.UNDER_THE_SEA)
     local insideTheBelly = player:getQuestStatus(xi.questLog.OTHER_AREAS, xi.quest.id.otherAreas.INSIDE_THE_BELLY)
-
-    -- UNDER THE SEA
-    if
-        underTheSea == xi.questStatus.QUEST_ACCEPTED and
-        not player:hasKeyItem(xi.ki.ETCHED_RING) and
-        npcUtil.tradeHas(trade, xi.item.FAT_GREEDIE)
-    then
-        if math.random(1, 100) <= 20 then
-            player:startEvent(35) -- Ring found !
-        else
-            player:startEvent(36) -- Ring not found
-        end
-
-    -- A BOY'S DREAM
-    elseif
-        player:getCharVar('aBoysDreamCS') == 5 and
-        npcUtil.tradeHasExactly(trade, xi.item.ODONTOTYRANNUS)
-    then
-        player:startEvent(85)
-
     -- INSIDE THE BELLY
-    elseif
+    if
         insideTheBelly == xi.questStatus.QUEST_ACCEPTED or
         insideTheBelly == xi.questStatus.QUEST_COMPLETED
     then
@@ -606,18 +585,7 @@ entity.onTrigger = function(player, npc)
     local insideTheBelly = player:getQuestStatus(xi.questLog.OTHER_AREAS, xi.quest.id.otherAreas.INSIDE_THE_BELLY)
     local mLvl           = player:getMainLvl()
 
-    -- UNDER THE SEA
-    if player:getCharVar('underTheSeaVar') == 3 then
-        player:startEvent(34, 4501) -- During quest 'Under the sea' - 3rd dialog
-
-    -- INSIDE THE BELLY
-    elseif
-        mLvl >= 30 and
-        theRealGift == xi.questStatus.QUEST_COMPLETED and
-        insideTheBelly == xi.questStatus.QUEST_AVAILABLE
-    then
-        player:startEvent(161)
-    elseif
+    if
         mLvl >= 30 and
         mLvl < 39 and
         (insideTheBelly == xi.questStatus.QUEST_ACCEPTED or insideTheBelly == xi.questStatus.QUEST_COMPLETED)
@@ -651,25 +619,8 @@ entity.onEventUpdate = function(player, csid, option, npc)
 end
 
 entity.onEventFinish = function(player, csid, option, npc)
-    -- UNDER THE SEA
-    if csid == 34 then
-        player:setCharVar('underTheSeaVar', 4)
-    elseif csid == 35 then
-        npcUtil.giveKeyItem(player, xi.ki.ETCHED_RING)
-        player:confirmTrade()
-    elseif csid == 36 then
-        player:confirmTrade()
-
-    -- A BOY'S DREAM
-    elseif csid == 85 then
-        npcUtil.giveKeyItem(player, xi.ki.KNIGHTS_BOOTS)
-        player:setCharVar('aBoysDreamCS', 6)
-        player:confirmTrade()
-
     -- INSIDE THE BELLY
-    elseif csid == 161 then
-        player:addQuest(xi.questLog.OTHER_AREAS, xi.quest.id.otherAreas.INSIDE_THE_BELLY)
-    elseif csid == 166 or csid == 167 then
+    if csid == 166 or csid == 167 then
         giveReward(player, csid)
     end
 end
