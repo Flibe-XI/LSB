9d3e088031d7846450a6f7f3697823bce058b6a9
Flibe-XI <paulbarron.ct@gmail.com>
2025-08-27 23:01:27 -0400

    Added pirates chart rewards

--- scripts/globals/pirates_chart.lua
+++ scripts/globals/pirates_chart.lua
@@ -342,6 +342,39 @@ xi.piratesChart.onItemCheck = function(target, item, param, caster)
     return xi.msg.basic.CANNOT_ON_THAT_TARG
 end
 
+local pChartLoot =
+{
+    {
+        { itemID = xi.item.CORAL_FRAGMENT,           weight = xi.loot.weight.VERY_LOW  }, --  4.3%
+        { itemID = xi.item.DRILL_CALAMARY,           weight = xi.loot.weight.NORMAL    }, -- 21.7%
+        { itemID = xi.item.DWARF_PUGIL,              weight = xi.loot.weight.LOW       }, -- 13.0%
+        { itemID = xi.item.HIGH_QUALITY_PUGIL_SCALE, weight = xi.loot.weight.VERY_LOW  }, --  4.3%
+        { itemID = xi.item.ONZ_OF_SALINATOR,         weight = xi.loot.weight.LOW       }, -- 13.0%
+        { itemID = xi.item.SHALL_SHELL,              weight = xi.loot.weight.VERY_HIGH }, -- 43.5%
+        { itemID = xi.item.ZEBRA_EEL,                weight = xi.loot.weight.LOW       }, -- 13.0%
+    },
+
+    {
+        { itemID = xi.item.ARROWWOOD_LOG,   weight = xi.loot.weight.HIGH     }, -- 21.2%
+        { itemID = xi.item.CORAL_BUTTERFLY, weight = xi.loot.weight.NORMAL   }, -- 15.2%
+        { itemID = xi.item.CORAL_FRAGMENT,  weight = xi.loot.weight.VERY_LOW }, --  3.0%
+        { itemID = xi.item.DRILL_CALAMARY,  weight = xi.loot.weight.NORMAL   }, -- 15.2%
+        { itemID = xi.item.DWARF_PUGIL,     weight = xi.loot.weight.NORMAL   }, -- 15.2%
+        { itemID = xi.item.NEBIMONITE,      weight = xi.loot.weight.LOW      }, --  9.1%
+        { itemID = xi.item.SHALL_SHELL,     weight = xi.loot.weight.HIGH     }, -- 21.2%
+    },
+
+    {
+        { itemID = xi.item.FUSCINA,          weight = xi.loot.weight.NORMAL        }, -- 80.6%
+        { itemID = xi.item.MERCURIAL_KRIS,   weight = xi.loot.weight.EXTREMELY_LOW }, --  3.2%
+        { itemID = xi.item.PIECE_OF_OXBLOOD, weight = xi.loot.weight.VERY_LOW      }, -- 16.1%
+    },
+
+    {
+        { xi.item.ALBATROSS_RING, weight = 1000 }, -- 100%
+    },
+}
+
 xi.piratesChart.barnacledBoxOnTrigger = function(player, npc)
     local qm4 = GetNPCByID(valkID.npc.PIRATE_CHART_QM)
 
@@ -360,7 +393,10 @@ xi.piratesChart.barnacledBoxOnTrigger = function(player, npc)
         npc:entityAnimationPacket(xi.animationString.OPEN_CRATE_GLOW)
         npc:setLocalVar(xi.animationString.OPEN_CRATE_GLOW, 1)
 
-        -- TODO: rewards
+        local rewards = utils.selectFromLootGroups(player, pChartLoot)
+        for _, item in ipairs(rewards) do
+            player:addTreasure(item, npc)
+        end
 
         npc:timer(15000, function(npcArg)
             npcArg:entityAnimationPacket(xi.animationString.STATUS_DISAPPEAR)
